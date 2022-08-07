6a23ea4e064c74b2a569bdeb10471473696ab637
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-08-07 23:23:13 +0900

    feat:should, find, eq, within used

---
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -0,0 +1,41 @@
+context('MyFirstTest', () => {
+  beforeEach(() => {
+    cy.visit('http://example.cypress.io/commands/actions')
+  })
+
+  it('has an h1 on the page', () => {
+    cy.get('h1').should('exist')
+  })
+
+  it('renders the correct h1 text', () => {
+    cy.get('h1').should('contain.text', 'Actions')
+  })
+
+  // 모든 container를 확인
+  it('renders the paragraph under the h1', () => {
+    cy.get('.container').find('p').should('exist')
+  })
+
+  // 첫번째 container만 확인
+  it('renders the paragraph under the h1', () => {
+    cy.get('.container').first().find('p').should('not.exist')
+  })
+
+  // n번째 container만 확인 eq뒤에 오는 값은 idx임 (0가능)
+  // 타켓도 1번째 container로 narrowed
+  // limits your action to that element
+  it('renders the paragraph under the h1', () => {
+    cy.get('.container').eq(2).find('p').should('exist')
+  })
+
+  // within은 find와 비슷.
+  // within을 하면 chaning the scope. In this case, I'm no longer targeting second container only. all
+  // changes the scope for DOM element of the passedFUnction to just child element.
+  // (아마) 해당 스코프내에서 더 많은 일을 할 수 있게 됨.
+  // 타겟은 2번째 container
+  it('renders a section with the correct elements', () => {
+    cy.get('.container').eq(2).within(() => {
+      cy.get('h4').should('exist')
+    })
+  })
+})
